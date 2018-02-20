fb1026874337344c8bd2bbd903093aba32ac680d
filiphamakers <filiphamakers@hotmail.com>
2018-02-20 08:48:48 +0100

    Class main in database (met code deze keer :p)

--- src/main/java/be/uzleuven/Quickstart.java
+++ src/main/java/be/uzleuven/Quickstart.java
@@ -1,24 +1,18 @@
 package be.uzleuven;
 
 import be.uzleuven.database.GoogleSlidesClient;
+import be.uzleuven.database.GoogleSlidesServiceInitializer;
 import be.uzleuven.makers.UZPresentation;
 import be.uzleuven.makers.UZSlide;
 import be.uzleuven.presentations.Presentation;
 import be.uzleuven.presentations.slides.Slide;
+import com.google.api.services.slides.v1.Slides;
 
 import java.io.IOException;
 
 public class Quickstart {
 
     public static void main(String[] args) throws IOException {
-        Presentation.Builder presentationBuilder = new Presentation.Builder();
 
-        Slide.Builder slideBuilder2 = new Slide.Builder();
-        Slide slide2 = slideBuilder2.setLayout(Slide.Layout.TITLE_AND_TWO_COLUMNS).build();
-        presentationBuilder.addSlide(slide2);
-
-        Presentation presentation = presentationBuilder
-                .build();
-        GoogleSlidesClient.save(presentation);
     }
 }
--- src/main/java/be/uzleuven/database/Main.java
+++ src/main/java/be/uzleuven/database/Main.java
@@ -1,4 +1,69 @@
 package be.uzleuven.database;
 
+import com.google.api.services.slides.v1.Slides;
+import com.google.api.services.slides.v1.model.*;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class Main {
+    public static void main(String[] args) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("MM/dd HH:mm");
+        String timeStamp = dtf.format(LocalDateTime.now()).toString();
+
+        Slides slidesService = GoogleSlidesServiceInitializer.getInstance();
+        Presentation presentation = new Presentation().setTitle(timeStamp);
+        List<Request> requests = new ArrayList<>(1);
+
+        try {
+            presentation = slidesService.presentations().create(presentation)
+                    .execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //make some blank slides
+        for (int i = 0; i < 3; i++) {
+            String shapeType = "TEXT_BOX";
+            if (i==1){
+                shapeType = "RECTANGLE";
+            }
+            if (i==2){
+                shapeType = "TRIANGLE";
+            }
+
+            CreateSlideRequest slide = new CreateSlideRequest();
+            CreateShapeRequest shape = new CreateShapeRequest().setObjectId(UUID.randomUUID().toString());
+            InsertTextRequest text = new InsertTextRequest().setObjectId(UUID.randomUUID().toString());
+
+            Dimension pt350 = new Dimension().setMagnitude(350.0).setUnit("PT");
+            shape.setShapeType(shapeType)
+                    .setElementProperties(new PageElementProperties()
+                            .setSize(new Size()
+                                    .setHeight(pt350)
+                                    .setWidth(pt350))
+                            .setPageObjectId(slide.getObjectId()));
+
+            text.setObjectId(shape.getObjectId()).setText("Dit is een test");
+
+            requests.add(new Request().setCreateSlide(slide));
+            requests.add(new Request().setCreateShape(shape));
+            requests.add(new Request().setInsertText(text));
+        }
+
+        BatchUpdatePresentationRequest body =
+                new BatchUpdatePresentationRequest().setRequests(requests);
+        try {
+            BatchUpdatePresentationResponse response =
+                    slidesService.presentations().batchUpdate(presentation.getPresentationId(), body).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
 }
